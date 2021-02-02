03d36426c76fe9cd04dfaa7b92f3bb6601335362
evriyanaindrasaputra <eindrasap@gmail.com>
2021-02-02 12:47:26 +0700

    favorit jokes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>
+  <div class="max-w-3xl w-11/12 md:w-full mx-auto">
     <div
-      class="home max-w-3xl w-full bg-green-300 mx-auto h-80 rounded-lg p-4 shadow-lg"
+      class="bg-green-300 h-60 rounded-lg p-4 shadow-lg mb-5 flex items-center"
     >
-      {{ joke.joke }}
+      <h2 class="font-bold text-2xl">{{ joke.joke }}</h2>
+    </div>
+    <div class="w-full grid grid-cols-2 justify-between items-center gap-x-5">
+      <button
+        class="bg-gradient-to-l from-yellow-200 to-green-400 p-3 rounded-md shadow-md w-full focus:outline-none transform hover:scale-105 ease-out duration-500 hover:from-green-400 hover:to-green-400"
+        @click="getJoke"
+      >
+        <p class="text-base font-medium text-black">Get Random Joke</p>
+      </button>
+      <button
+        class="bg-gradient-to-r from-red-300 to-pink-400 p-3 rounded-md shadow-md w-full focus:outline-none transform hover:scale-105 ease-out duration-500 hover:from-pink-400 hover:to-pink-400"
+        @click="AdtoFavorit"
+      >
+        <p class="text-base font-medium text-black">Add to Favorit</p>
+      </button>
     </div>
   </div>
 </template>
@@ -15,13 +29,18 @@ const headers = { Accept: "application/json" };
 export default {
   name: 'Home',
   data: () => ({
-    joke : []
+    joke : [],
+    favoritJokes : []
   }),
   methods: {
     getJoke(){
       fetch(url, {headers}).then(response => response.json()).then(data => {
         this.joke = data
       })
+    },
+    AdtoFavorit() {
+      const newJoke = this.joke
+      this.favoritJokes.push(newJoke)
     }
   },
   mounted(){
